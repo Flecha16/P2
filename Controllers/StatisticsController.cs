47bc666e595dab88ed41b1636f3c39831fcaaf15
Flecha16 <juanflecha16@outlook.com>
2023-06-14 18:41:49 -0500

    Statistics 2

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using AuthSystem.Data;
 using AuthSystem.Migrations;
 using AuthSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Player = AuthSystem.Models.Player;
+using Position = AuthSystem.Models.Position;
 
 namespace AuthSystem.Controllers
 {
     public class StatisticsController : Controller
     {
         private readonly AuthDbContext _context;
+        double weightGoals;
+        double weightAssists;
+        double weightMatchesPlayed;
+        double weightTotalKm;
+        double weightAverageSpeed;
 
         public StatisticsController(AuthDbContext context)
         {
@@ -28,6 +37,15 @@ namespace AuthSystem.Controllers
             return View(statistics);
         }
 
+        public async Task<IActionResult> Details (int? id)
+        {
+            var existingStatistic = await _context.Statistics
+                .Include(s => s.Player)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
         // GET: Statistics/Create
         public IActionResult Create()
         {
@@ -36,24 +54,135 @@ namespace AuthSystem.Controllers
             return View();
         }
 
-        // POST: Statistics/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Statistic statistic)
         {
-
-            // Obtener el jugador seleccionado
+            // Buscar la estadística existente del jugador
+            var existingStatistic = await _context.Statistics.FirstOrDefaultAsync(s => s.PlayerId == statistic.PlayerId);
             var player = await _context.Players.FindAsync(statistic.PlayerId);
-
             statistic.TeamId = player.TeamId;
             statistic.Player = player;
 
-            _context.Statistics.Add(statistic);
-            _context.SaveChanges();
-            return RedirectToAction("Index", "Statistics");
+            if (existingStatistic != null)
+            {
+                existingStatistic.Matches += statistic.Matches;
+                existingStatistic.Minutes += statistic.Minutes;
+                existingStatistic.Goals += statistic.Goals;
+                existingStatistic.Assists += statistic.Assists;
+                existingStatistic.YellowCards += statistic.YellowCards;
+                existingStatistic.RedCards += statistic.RedCards;
+                existingStatistic.Km += statistic.Km;
 
+                if (existingStatistic.Player.Position == Position.GK)
+                {
+                    existingStatistic.AverageSpeed += statistic.AverageSpeed;
+                }
+                else
+                {
+                    if (existingStatistic.AverageSpeed == 0)
+                    {
+                        existingStatistic.AverageSpeed += statistic.AverageSpeed;
+                    }
+                    else
+                    {
+                        existingStatistic.AverageSpeed = (existingStatistic.AverageSpeed + statistic.AverageSpeed) / 2;
+                    }
+
+                }
+
+                if (statistic.Player.Position == Position.LW || statistic.Player.Position == Position.RW ||
+                    statistic.Player.Position == Position.CF || statistic.Player.Position == Position.ST)
+                {
+                    weightGoals = 0.4;
+                    weightAssists = 0.3;
+                    weightMatchesPlayed = 0.1;
+                    weightTotalKm = 0.1;
+                    weightAverageSpeed = 0.1;
+                }
+                else if (statistic.Player.Position == Position.CDM || statistic.Player.Position == Position.CM ||
+                    statistic.Player.Position == Position.LM || statistic.Player.Position == Position.RM ||
+                    statistic.Player.Position == Position.CAM)
+                {
+                    weightGoals = 0.2;
+                    weightAssists = 0.4;
+                    weightMatchesPlayed = 0.2;
+                    weightTotalKm = 0.1;
+                    weightAverageSpeed = 0.1;
+
+                }
+                else if (statistic.Player.Position == Position.CB || statistic.Player.Position == Position.LB ||
+                    statistic.Player.Position == Position.RB || statistic.Player.Position == Position.SW)
+                {
+                    weightGoals = 0.2;
+                    weightAssists = 0.3;
+                    weightMatchesPlayed = 0.2;
+                    weightTotalKm = 0.2;
+                    weightAverageSpeed = 0.1;
+
+                }
+                else if (statistic.Player.Position == Position.GK)
+                {
+                    weightGoals = 0.1;
+                    weightAssists = 0.1;
+                    weightMatchesPlayed = 0.6;
+                    weightTotalKm = 0.1;
+                    weightAverageSpeed = 0.1;
+
+                }
+
+                // Calcula la media ponderada
+                double playerAverage = (existingStatistic.Goals * weightGoals +
+                                 existingStatistic.Assists * weightAssists +
+                                 existingStatistic.Matches * weightMatchesPlayed +
+                                 (existingStatistic.Km / existingStatistic.Matches) * weightTotalKm +
+                                 existingStatistic.AverageSpeed * weightAverageSpeed);
+
+                // Aplica el límite superior de 100
+                playerAverage = Math.Min(playerAverage, 100);
+
+                existingStatistic.Player.Valoration = (int)playerAverage;
+                _context.Update(existingStatistic);
+                await _context.SaveChangesAsync();
+
+            }
+            else
+            {
+                // Obtener el jugador seleccionado
+                player = await _context.Players.FindAsync(statistic.PlayerId);
+
+                statistic.TeamId = player.TeamId;
+                statistic.Player = player;
+
+                weightGoals = 0.4;
+                weightAssists = 0.3;
+                weightMatchesPlayed = 0.1;
+                weightTotalKm = 0.1;
+                weightAverageSpeed = 0.1;
+
+                // Calcula la media ponderada
+                double playerAverage = (statistic.Goals * weightGoals +
+                                 statistic.Assists * weightAssists +
+                                 statistic.Matches * weightMatchesPlayed +
+                                 (statistic.Km / statistic.Matches) * weightTotalKm +
+                                 statistic.AverageSpeed * weightAverageSpeed);
+
+                // Aplica el límite superior de 100
+                playerAverage = Math.Min(playerAverage, 100);
+
+                statistic.Player.Valoration = (int)playerAverage;
+
+                _context.Statistics.Add(statistic);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Statistics");
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
 
+
+        [Authorize(Roles = "Admin")]
         // GET: Statistics/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -96,35 +225,73 @@ namespace AuthSystem.Controllers
                     existingStatistic.Assists += updatedStatistic.Assists;
                     existingStatistic.YellowCards += updatedStatistic.YellowCards;
                     existingStatistic.RedCards += updatedStatistic.RedCards;
-                    if (existingStatistic.AverageSpeed == 0)
+                    existingStatistic.Km += updatedStatistic.Km;
+
+                    if (existingStatistic.Player.Position == Position.GK)
                     {
                         existingStatistic.AverageSpeed += updatedStatistic.AverageSpeed;
                     }
                     else
                     {
-                        existingStatistic.AverageSpeed = (existingStatistic.AverageSpeed + updatedStatistic.AverageSpeed) / 2;
+                        if (existingStatistic.AverageSpeed == 0)
+                        {
+                            existingStatistic.AverageSpeed += updatedStatistic.AverageSpeed;
+                        }
+                        else
+                        {
+                            existingStatistic.AverageSpeed = (existingStatistic.AverageSpeed + updatedStatistic.AverageSpeed) / 2;
+                        }
                     }
-                    
-                    existingStatistic.Km += updatedStatistic.Km;
-                    
-                    double weightGoals = 0.4;
-                    double weightAssists = 0.3;
-                    double weightMatchesPlayed = 0.1;
-                    double weightTotalKm = 0.1;
-                    double weightAverageSpeed = 0.1;
+                    if (existingStatistic.Player.Position == Position.LW || existingStatistic.Player.Position == Position.RW ||
+                        existingStatistic.Player.Position == Position.CF || existingStatistic.Player.Position == Position.ST)
+                    {
+                        weightGoals = 0.4;
+                        weightAssists = 0.3;
+                        weightMatchesPlayed = 0.1;
+                        weightTotalKm = 0.1;
+                        weightAverageSpeed = 0.1;
+                    }
+                    else if (existingStatistic.Player.Position == Position.CDM || existingStatistic.Player.Position == Position.CM ||
+                    existingStatistic.Player.Position == Position.LM || existingStatistic.Player.Position == Position.RM ||
+                        existingStatistic.Player.Position == Position.CAM)
+                    {
+                        weightGoals = 0.2;
+                        weightAssists = 0.4;
+                        weightMatchesPlayed = 0.2;
+                        weightTotalKm = 0.1;
+                        weightAverageSpeed = 0.1;
+                    }
+                    else if (existingStatistic.Player.Position == Position.CB || existingStatistic.Player.Position == Position.LB ||
+                        existingStatistic.Player.Position == Position.RB || existingStatistic.Player.Position == Position.SW)
+                    {
+                        weightGoals = 0.2;
+                        weightAssists = 0.3;
+                        weightMatchesPlayed = 0.2;
+                        weightTotalKm = 0.2;
+                        weightAverageSpeed = 0.1;
+
+                    }
+                    else if (existingStatistic.Player.Position == Position.GK)
+                    {
+                        weightGoals = 0.1;
+                        weightAssists = 0.1;
+                        weightMatchesPlayed = 0.6;
+                        weightTotalKm = 0.1;
+                        weightAverageSpeed = 0.1;
+
+                    }
 
                     // Calcula la media ponderada
                     double playerAverage = (existingStatistic.Goals * weightGoals +
                                      existingStatistic.Assists * weightAssists +
                                      existingStatistic.Matches * weightMatchesPlayed +
-                                     (existingStatistic.Km/existingStatistic.Matches) * weightTotalKm +
+                                     (existingStatistic.Km / existingStatistic.Matches) * weightTotalKm +
                                      existingStatistic.AverageSpeed * weightAverageSpeed);
 
                     // Aplica el límite superior de 100
                     playerAverage = Math.Min(playerAverage, 100);
 
-                    existingStatistic.Player.Valoration = (int) playerAverage;
-
+                    existingStatistic.Player.Valoration = (int)playerAverage;
                     _context.Update(existingStatistic);
                     await _context.SaveChangesAsync();
                 }
@@ -143,6 +310,7 @@ namespace AuthSystem.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [Authorize(Roles = "Admin")]
         // GET: Statistics/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -176,27 +344,6 @@ namespace AuthSystem.Controllers
             return _context.Statistics.Any(e => e.Id == id);
         }
 
-        double CalculatePlayerAverage(int goals, int assists, int matchesPlayed, double totalKm, double averageSpeed)
-        {
-            // Asigna los pesos para cada estadística
-            double weightGoals = 0.4;
-            double weightAssists = 0.3;
-            double weightMatchesPlayed = 0.1;
-            double weightTotalKm = 0.1;
-            double weightAverageSpeed = 0.1;
-
-            // Calcula la media ponderada
-            double average = (goals * weightGoals +
-                             assists * weightAssists +
-                             matchesPlayed * weightMatchesPlayed +
-                             totalKm * weightTotalKm +
-                             averageSpeed * weightAverageSpeed);
-
-            // Aplica el límite superior de 100
-            average = Math.Min(average, 100);
-
-            return average;
-        }
 
     }
 
